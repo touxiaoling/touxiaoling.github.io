ec859b897d8962e6b1b9cf5772621ff2569b6be7
tomin <touxiaoling@outlook.com>
2022-10-07 14:55:05 +0800

    优化修改时间脚本

--- lib/fix_mtime.py
+++ lib/fix_mtime.py
@@ -11,4 +11,7 @@ for p in doc_path.glob("**/*.md"):
     if not m_time:
         continue
     m_time=int(m_time.decode().strip())
-    os.utime(p,(m_time,m_time))
+    m_time_now=int(p.stat().st_mtime)
+    if m_time_now!=m_time:
+        print(f"change {p}'s mtime from {datetime.fromtimestamp(m_time_now)} to {datetime.fromtimestamp(m_time)}")
+        os.utime(p,(m_time,m_time))
