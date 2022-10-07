60a0837ea01c050faba51f0057dba0444b0b645c
tomin <touxiaoling@outlook.com>
2022-10-07 17:30:38 +0800

    add add_filehistory.py

---
+++ lib/add_filehistory.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from importlib.resources import contents
+import subprocess
+from pathlib import Path
+import os
+doc_path=Path("./source/")
+
+for p in doc_path.glob("**/*.md"):
+    if p.stem=="index":
+        continue
+    change_info=subprocess.check_output(f'git log --pretty=format:"%as | %s" -- {p} ',shell=True)
+    print(change_info.decode().split("\n"))
+    change_log = '''\n## 修改历史
+|  时间  | 修改内容 |
+| ---- | --- |\n'''
+    for i_change in change_info:
+        change_log+=i_change+"\n"
+    
+    context=p.read_text()+change_log
+    p.write_text(context)
